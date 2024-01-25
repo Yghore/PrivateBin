7dee015ca283aae23bfe005dcca6d4f92ea51632
Yghore <Yhgore@gmail.com>
2024-01-25 09:33:21 +0100

    Test

--- tst/FilterTest.php
+++ tst/FilterTest.php
@@ -1,16 +1,30 @@
 <?php
 
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use PHPUnit\Framework\TestCase;
 use PrivateBin\Filter;
 
-class FilterTest extends TestCase
+/**
+ * @internal
+ * @coversNothing
+ */
+final class FilterTest extends TestCase
 {
     public function testFilterMakesTimesHumanlyReadable()
     {
-        $this->assertEquals('5 minutes', Filter::formatHumanReadableTime('5min'));
-        $this->assertEquals('90 seconds', Filter::formatHumanReadableTime('90sec'));
-        $this->assertEquals('1 week', Filter::formatHumanReadableTime('1week'));
-        $this->assertEquals('6 months', Filter::formatHumanReadableTime('6months'));
+        static::assertSame('5 minutes', Filter::formatHumanReadableTime('5min'));
+        static::assertSame('90 seconds', Filter::formatHumanReadableTime('90sec'));
+        static::assertSame('1 week', Filter::formatHumanReadableTime('1week'));
+        static::assertSame('6 months', Filter::formatHumanReadableTime('6months'));
     }
 
     public function testFilterFailTimesHumanlyReadable()
@@ -22,37 +36,37 @@ class FilterTest extends TestCase
 
     public function testFilterMakesSizesHumanlyReadable()
     {
-        $this->assertEquals('1 B', Filter::formatHumanReadableSize(1));
-        $this->assertEquals('1 000 B', Filter::formatHumanReadableSize(1000));
-        $this->assertEquals('1.00 KiB', Filter::formatHumanReadableSize(1024));
-        $this->assertEquals('1.21 KiB', Filter::formatHumanReadableSize(1234));
+        static::assertSame('1 B', Filter::formatHumanReadableSize(1));
+        static::assertSame('1 000 B', Filter::formatHumanReadableSize(1000));
+        static::assertSame('1.00 KiB', Filter::formatHumanReadableSize(1024));
+        static::assertSame('1.21 KiB', Filter::formatHumanReadableSize(1234));
         $exponent = 1024;
-        $this->assertEquals('1 000.00 KiB', Filter::formatHumanReadableSize(1000 * $exponent));
-        $this->assertEquals('1.00 MiB', Filter::formatHumanReadableSize(1024 * $exponent));
-        $this->assertEquals('1.21 MiB', Filter::formatHumanReadableSize(1234 * $exponent));
+        static::assertSame('1 000.00 KiB', Filter::formatHumanReadableSize(1000 * $exponent));
+        static::assertSame('1.00 MiB', Filter::formatHumanReadableSize(1024 * $exponent));
+        static::assertSame('1.21 MiB', Filter::formatHumanReadableSize(1234 * $exponent));
         $exponent *= 1024;
-        $this->assertEquals('1 000.00 MiB', Filter::formatHumanReadableSize(1000 * $exponent));
-        $this->assertEquals('1.00 GiB', Filter::formatHumanReadableSize(1024 * $exponent));
-        $this->assertEquals('1.21 GiB', Filter::formatHumanReadableSize(1234 * $exponent));
+        static::assertSame('1 000.00 MiB', Filter::formatHumanReadableSize(1000 * $exponent));
+        static::assertSame('1.00 GiB', Filter::formatHumanReadableSize(1024 * $exponent));
+        static::assertSame('1.21 GiB', Filter::formatHumanReadableSize(1234 * $exponent));
         $exponent *= 1024;
-        $this->assertEquals('1 000.00 GiB', Filter::formatHumanReadableSize(1000 * $exponent));
-        $this->assertEquals('1.00 TiB', Filter::formatHumanReadableSize(1024 * $exponent));
-        $this->assertEquals('1.21 TiB', Filter::formatHumanReadableSize(1234 * $exponent));
+        static::assertSame('1 000.00 GiB', Filter::formatHumanReadableSize(1000 * $exponent));
+        static::assertSame('1.00 TiB', Filter::formatHumanReadableSize(1024 * $exponent));
+        static::assertSame('1.21 TiB', Filter::formatHumanReadableSize(1234 * $exponent));
         $exponent *= 1024;
-        $this->assertEquals('1 000.00 TiB', Filter::formatHumanReadableSize(1000 * $exponent));
-        $this->assertEquals('1.00 PiB', Filter::formatHumanReadableSize(1024 * $exponent));
-        $this->assertEquals('1.21 PiB', Filter::formatHumanReadableSize(1234 * $exponent));
+        static::assertSame('1 000.00 TiB', Filter::formatHumanReadableSize(1000 * $exponent));
+        static::assertSame('1.00 PiB', Filter::formatHumanReadableSize(1024 * $exponent));
+        static::assertSame('1.21 PiB', Filter::formatHumanReadableSize(1234 * $exponent));
         $exponent *= 1024;
-        $this->assertEquals('1 000.00 PiB', Filter::formatHumanReadableSize(1000 * $exponent));
-        $this->assertEquals('1.00 EiB', Filter::formatHumanReadableSize(1024 * $exponent));
-        $this->assertEquals('1.21 EiB', Filter::formatHumanReadableSize(1234 * $exponent));
+        static::assertSame('1 000.00 PiB', Filter::formatHumanReadableSize(1000 * $exponent));
+        static::assertSame('1.00 EiB', Filter::formatHumanReadableSize(1024 * $exponent));
+        static::assertSame('1.21 EiB', Filter::formatHumanReadableSize(1234 * $exponent));
         $exponent *= 1024;
-        $this->assertEquals('1 000.00 EiB', Filter::formatHumanReadableSize(1000 * $exponent));
-        $this->assertEquals('1.00 ZiB', Filter::formatHumanReadableSize(1024 * $exponent));
-        $this->assertEquals('1.21 ZiB', Filter::formatHumanReadableSize(1234 * $exponent));
+        static::assertSame('1 000.00 EiB', Filter::formatHumanReadableSize(1000 * $exponent));
+        static::assertSame('1.00 ZiB', Filter::formatHumanReadableSize(1024 * $exponent));
+        static::assertSame('1.21 ZiB', Filter::formatHumanReadableSize(1234 * $exponent));
         $exponent *= 1024;
-        $this->assertEquals('1 000.00 ZiB', Filter::formatHumanReadableSize(1000 * $exponent));
-        $this->assertEquals('1.00 YiB', Filter::formatHumanReadableSize(1024 * $exponent));
-        $this->assertEquals('1.21 YiB', Filter::formatHumanReadableSize(1234 * $exponent));
+        static::assertSame('1 000.00 ZiB', Filter::formatHumanReadableSize(1000 * $exponent));
+        static::assertSame('1.00 YiB', Filter::formatHumanReadableSize(1024 * $exponent));
+        static::assertSame('1.21 YiB', Filter::formatHumanReadableSize(1234 * $exponent));
     }
 }
